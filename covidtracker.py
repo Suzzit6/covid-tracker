bb16bec3626d7956e0db59930082bf3cc6719e1b
sujit mishra <mishrasujit409@gmail.com>
2024-10-13 14:55:11 +0530

    function made

--- covidtracker.py
+++ covidtracker.py
@@ -6,31 +6,30 @@ from datetime import date
 import pandas as pd
 import matplotlib.pyplot as plt
 
+def LineGraph(country,df,action):
+   country_rows = df[df["Country/Region"] == country]
+   country_rows["Date"] = pd.to_datetime(country_rows["Date"])
+   country_rows["year_month"] = country_rows["Date"].dt.to_period("M")
+   monthly_deaths_in = country_rows.groupby("year_month")[action].sum().reset_index()
+   monthly_deaths_in["year_month"] = monthly_deaths_in["year_month"].dt.to_timestamp()
+   return monthly_deaths_in 
+
 df = pd.read_csv("full_grouped.csv")
 
 df['Date'] = pd.to_datetime(df['Date'])
-india_rows = df[df["Country/Region"] == "India"]
-# deaths_india = india_rows.groupby("Date")["Deaths"].sum()
-india_rows["Date"] = pd.to_datetime(india_rows["Date"])
 
-india_rows["year_month"] = india_rows["Date"].dt.to_period("M")
-monthly_deaths_in = india_rows.groupby("year_month")['Deaths'].sum().reset_index()
-monthly_deaths_in["year_month"] = monthly_deaths_in["year_month"].dt.to_timestamp()
 # print(monthly_deaths)
+monthly_deaths_in = LineGraph("India",df,"Confirmed")
+monthly_deaths_us = LineGraph("US",df,"Confirmed")
+# us_rows = df[df["Country/Region"] == "US"]
+# us_rows["Date"] = pd.to_datetime(us_rows["Date"])
+# us_rows["year_month"] = us_rows["Date"].dt.to_period("M")
+# monthly_deaths_us = us_rows.groupby("year_month")["Deaths"].sum().reset_index()
+# monthly_deaths_us["year_month"] = monthly_deaths_us["year_month"].dt.to_timestamp()
 
-us_rows = df[df["Country/Region"] == "US"]
-us_rows["Date"] = pd.to_datetime(us_rows["Date"])
-us_rows["year_month"] = us_rows["Date"].dt.to_period("M")
-monthly_deaths_us = us_rows.groupby("year_month")["Deaths"].sum().reset_index()
-monthly_deaths_us["year_month"] = monthly_deaths_us["year_month"].dt.to_timestamp()
-
-plt.plot(monthly_deaths_in["year_month"], monthly_deaths_in['Deaths'], label="India", color="orange")
-plt.plot(monthly_deaths_us["year_month"], monthly_deaths_us['Deaths'], label="USA", color="blue")
+plt.plot(monthly_deaths_in["year_month"], monthly_deaths_in['Confirmed'], label="India", color="orange")
+plt.plot(monthly_deaths_us["year_month"], monthly_deaths_us['Confirmed'], label="USA", color="blue")
 plt.xlabel("Months")
-plt.ylabel("Deaths")
+plt.ylabel("Confirmed")
 plt.legend()
 plt.show()
-# def Get_Deaths(year,country,df):
-#     {
-# 
-# }
